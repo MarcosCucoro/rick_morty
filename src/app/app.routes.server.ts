3606ea9b2be955758e26d2e93584b2dd7c71b0e0
Marcos Cucoro <marcos.cucoro@refit.com.br>
2025-10-30 15:21:46 -0300

    feat: Add server routes configuration and Vercel deployment settings

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,48 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'login',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'home',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'character',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'character/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'location',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'location/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'episode',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'episode/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'profile-user',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Server
-  }
+  { path: '', renderMode: RenderMode.Server },
+  { path: 'login', renderMode: RenderMode.Server },
+  { path: 'home', renderMode: RenderMode.Server },
+  { path: 'character', renderMode: RenderMode.Server },
+  { path: 'character/:id', renderMode: RenderMode.Server },
+  { path: 'location', renderMode: RenderMode.Server },
+  { path: 'location/:id', renderMode: RenderMode.Server },
+  { path: 'episode', renderMode: RenderMode.Server },
+  { path: 'episode/:id', renderMode: RenderMode.Server },
+  { path: 'profile-user', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server },
 ];
